32e9bee109f43beaf39de1b39906f69286dae6f3
gezza <gerardmccann94@hotmail.co.uk>
2024-06-27 16:52:19 +0100

    ADD: Image templ components added

--- handler/generate.go
+++ handler/generate.go
@@ -1,10 +1,32 @@
 package handler
 
 import (
+	"log/slog"
 	"net/http"
+	"picturethisai/types"
 	"picturethisai/view/generate"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func HandleGenerateIndex(w http.ResponseWriter, r *http.Request) error {
-	return render(r, w, generate.Index())
+	data := generate.ViewData{
+		Images: []types.Image{},
+	}
+	return render(r, w, generate.Index(data))
+}
+
+func HandleGenerateCreate(w http.ResponseWriter, r *http.Request) error {
+
+	return render(r, w, generate.GalleryImage(types.Image{Status: types.ImageStatusPending}))
+}
+
+func HandleGenerateImageStatus(w http.ResponseWriter, r *http.Request) error {
+	id := chi.URLParam(r, "id")
+	// Fetch image from DB
+	image := types.Image{
+		Status: types.ImageStatusPending,
+	}
+	slog.Info("checking image status:", "id", id)
+	return render(r, w, generate.GalleryImage(image))
 }
